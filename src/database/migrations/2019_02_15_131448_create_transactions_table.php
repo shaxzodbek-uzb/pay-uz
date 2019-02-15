c550db4c45ad9677f5f94f9d12e560bd3911c166
azizbekeshonaliyev <1996azizbekeshonaliyev@gmail.com>
2019-02-15 21:14:18 +0500

    database

---
+++ src/database/migrations/2019_02_15_131448_create_transactions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('payment_system');
+            $table->string('system_transaction_id');
+            $table->double('amount',15,5);
+            $table->integer('currency_code');
+            $table->string('payable')->nullable();
+            $table->integer('payable_id')->nullable();
+            $table->integer('state');
+            $table->dateTime('create_time')->nullable();
+            $table->dateTime('cancel_time')->nullable();
+            $table->dateTime('perform_time')->nullable();
+            $table->dateTime('system_time_datetime')->nullable();
+            $table->string('comment')->nullable();
+            $table->string('detail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
