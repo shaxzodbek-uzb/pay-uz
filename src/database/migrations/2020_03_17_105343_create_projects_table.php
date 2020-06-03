f02289ca974b3b9b04acceff51f79c6a0a4337ec
shaxzodbek-uzb <47610909+shaxzodbek-uzb@users.noreply.github.com>
2020-06-03 15:35:46 +0500

    Update 2020_03_17_105343_create_projects_table.php

--- src/database/migrations/2020_03_17_105343_create_projects_table.php
+++ src/database/migrations/2020_03_17_105343_create_projects_table.php
@@ -14,7 +14,7 @@ class CreateProjectsTable extends Migration
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
-            $table->increments();
+            $table->increments('id');
             $table->string('name');
             $table->integer('status');
             $table->jsonb('details');
